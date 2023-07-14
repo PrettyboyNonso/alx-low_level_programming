cfa01f4b2357e5c8ba857f39017cc4e47f3685bc
Darlington <obii6776@gmail.com>
2023-07-13 18:49:42 -0700

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * main - entry point
+ * Return : 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
