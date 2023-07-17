266bef9c847febaca97a0940c142b0421764e448
Darlington <obii6776@gmail.com>
2023-07-17 15:10:32 -0700

    omit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: omit alphabets
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char lowcase;
+
+lowcase = 'a';
+while (lowcase < 123)
+{
+if (lowcase == 101 || lowcase == 113)
+{
+lowcase++;
+continue;
+}
+putchar(lowcase);
+lowcase++;
+}
+
+return (0);
+}
