e4350e9887600f2b25d790577ceb876050fde9e2
Darlington <obii6776@gmail.com>
2023-07-17 15:31:22 -0700

    zero to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: zero to nine
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+
+x = 0;
+for (x = 0; x <= 9; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
