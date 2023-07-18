4737802e73783555a9244e9619c12a52385e635b
Darlington <obii6776@gmail.com>
2023-07-18 11:45:09 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int printout(void)
+{
+	printf("_putchar");
+	return (0);
+
+}
+
+int main(void)
+{
+	printout();
+	return (0);
+
+}
