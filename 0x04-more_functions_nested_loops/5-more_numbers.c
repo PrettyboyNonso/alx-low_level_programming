bd914c41b949b36f3700a22b53f681c0456fa445
Darlington <obii6776@gmail.com>
2023-07-20 14:13:05 -0700

    5 numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,9 +16,12 @@ void more_numbers(void)
 	{
 	for (j = 0; j <= 14; j++)
 	{
-	_putchar(j + '0');
+	if (j > 9)
+	{
+	_putchar('1');
+	}
+	_putchar('0' + (j % 10));
 	}
 	_putchar('\n');
 	}
-	_putchar('\n');
 }
