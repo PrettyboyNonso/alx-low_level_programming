903ff8b286345ba39991b6a924019a037dd0cb09
Darlington <obii6776@gmail.com>
2023-07-20 16:38:18 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * fizzbuzz - Entry point
+ *
+ * @void: function parameter is empty
+ *
+ * Return: always 0
+ */
+void fizzbuzz(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 ==0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+}
