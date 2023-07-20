eb7080a9b391f064c1c4c5491d6c66bbae9e10dd
Darlington <obii6776@gmail.com>
2023-07-20 07:28:08 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * @void:  empty parameter
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
