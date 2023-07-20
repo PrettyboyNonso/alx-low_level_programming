a0eaf02d76aa445a828ce1dc9b6320ca799c9822
Darlington <obii6776@gmail.com>
2023-07-19 17:57:43 -0700

    betty please

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point 
+ *
+ * Description: prints 10x
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char first;
+
+	first = 'a';
+	for (a = 0; a <= 9; a++)
+	{
+		for (first = 'a'; first <= 'z'; first++)
+		{
+			_putchar(first);
+		
+		}
+		_putchar('\n');
+	}
+}
