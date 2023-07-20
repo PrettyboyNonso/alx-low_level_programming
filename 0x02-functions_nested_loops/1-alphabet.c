ec6500732fe05f8e82351ec89fa02e6d77fdb341
Darlington <obii6776@gmail.com>
2023-07-19 17:30:15 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char first;
+	
+	first = 'a';
+	while (first <= 'z')
+	{
+		_putchar(first);
+		first++;
+	
+	}
+
+	_putchar('\n');
+
+
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabets
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+
+}
