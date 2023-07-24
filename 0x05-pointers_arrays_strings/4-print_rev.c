9a73b3891a13f5d429110b719f6f13926eed2596
Darlington <obii6776@gmail.com>
2023-07-24 14:03:45 -0700

    backward

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - Entry of function
+ *
+ * @s: function parameter
+ *
+ * Return: always 0
+ */
+void print_rev(char *s)
+{
+	int length;
+	int i;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+
+	
+	for (i = length - 1; i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+}
