f01a8c878ace41f63bd4edf935934b7e9d218039
Darlington <obii6776@gmail.com>
2023-07-24 12:36:01 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Swap variable
+ *
+ * @a: function parameter
+ *
+ * @b: function parameter
+ *
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
