a1a789bc604698dbf00c9739d580b586875a9b1e
Darlington <obii6776@gmail.com>
2023-07-24 18:38:33 -0700

    i am getting tired

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,9 +9,10 @@
 
 int _atoi(char *s)
 {
-	long sign = 1;
-	long result = 0;
-	long digit;
+	int sign = 1;
+	int result = 0;
+	int digit;
+	int prev_result;
 
 	while (*s)
 	{
@@ -20,7 +21,16 @@ int _atoi(char *s)
 	else if (*s >= '0' && *s <= '9')
 	{
 	digit = *s - '0';
+	prev_result = result;
 	result = result * 10 + digit;
+
+	if (result < prev_result)
+	{
+	if (sign == 1)
+	return INT_MAX;
+	else
+	return INT_MIN;
+	}
 	}
 	else if (result != 0)
 	break;
