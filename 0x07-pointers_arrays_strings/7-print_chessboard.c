acfd877c59d4a4d1947bda676c7df578d3c87189
Darlington <obii6776@gmail.com>
2023-07-31 11:49:37 -0700

    chessboard3

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int row;
-	int col;
+	int row, col;
 
 	for (row = 0; row < 8; row++)
 	{
 	for (col = 0; col < 8; col++)
 	{
-	printf("%c ", a[row][col]);
+	_putchar(a[row][col]);
+	_putchar(' ');
 	}
-	printf("\n");
+	_putchar('\n');
 	}
 }
