48c771a3827896491085ef67d2162d23c67c5d32
Darlington <obii6776@gmail.com>
2023-07-31 12:06:08 -0700

    done

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,38 +1,21 @@
 #include "main.h"
-#include <stdio.h>
-#include "main.h"
 
 /**
- * print_diagsums - Prints the sum of the two diagonals of a square matrix.
- * @a: Pointer to the 2D array representing the square matrix.
- * @size: Size of the square matrix.
+ * print_chessboard - Prints the chessboard.
+ * @board: The 2D array representing the chessboard.
  *
  * Return: void.
  */
-void print_diagsums(int *a, int size)
+void print_chessboard(char (*board)[8])
 {
-	int sum_main = 0;
-	int sum_secondary = 0;
-	int i;
+	int row, col;
 
-
-	for (i = 0; i < size; i++)
+	for (row = 0; row < 8; row++)
 	{
-		sum_main += a[i * size + i];
-		sum_secondary += a[i * size + (size - 1 - i)];
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(board[row][col]);
+		}
+		_putchar('\n');
 	}
-
-
-	_putchar(sum_main / 1000 + '0');
-	_putchar((sum_main / 100) % 10 + '0');
-	_putchar((sum_main / 10) % 10 + '0');
-	_putchar(sum_main % 10 + '0');
-	_putchar(',');
-	_putchar(' ');
-	_putchar(sum_secondary / 1000 + '0');
-	_putchar((sum_secondary / 100) % 10 + '0');
-	_putchar((sum_secondary / 10) % 10 + '0');
-	_putchar(sum_secondary % 10 + '0');
-	_putchar('\n');
 }
-
