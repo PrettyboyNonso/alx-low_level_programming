c4078d44bba1ec790a6959995d052d6ee2f407d6
Darlington <obii6776@gmail.com>
2023-08-03 02:04:10 -0700

    recursion 2.0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-      _putchar(*s);
+	if (*s != '\0')
+	{
+	_putchar(*s);
+	_putchar('\n');
 
-        _puts_recursion(s + 1);
-    }
+	_puts_recursion(s + 1)
+	}
 }
 
