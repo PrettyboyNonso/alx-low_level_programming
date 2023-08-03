65c36ac6ffa91a20f9a240f4dba25870396d4f4c
Darlington <obii6776@gmail.com>
2023-08-03 06:44:07 -0700

    testing

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (find_sqrt(n, 0, n));
+}
+
+int find_sqrt(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
+
+	if (start <= end)
+	{
+		int square = mid * mid;
+		if (square == n)
+		{
+			return (mid);
+		}
+		else if (square < n)
+		{
+			return (find_sqrt(n, mid + 1, end));
+		}
+		else
+		{
+			return (find_sqrt(n, start, mid - 1));
+		}
+	}
+	return (end);
+}
