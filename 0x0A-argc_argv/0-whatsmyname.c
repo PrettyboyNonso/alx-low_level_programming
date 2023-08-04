158019ea6cbc86c0064b2a0f865bd3226c2a078f
Darlington <obii6776@gmail.com>
2023-08-04 16:09:14 -0700

    testing

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include "main.h"
+/**
+ * main - Entry point
+ * @argv: param
+ * @argc: param
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
 _putchar(argv[0]);
+_putchar('\n');
+return (0);
 }
