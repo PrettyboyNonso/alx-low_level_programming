ad1017b4b1027c115526a00219242a461066fe3d
Darlington <obii6776@gmail.com>
2023-08-04 18:33:14 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,17 @@ int one;
 int two;
 int result;
 
-if (argc < 2)
+if (argc !=  2)
 {
 printf("Error\n");
 return (1);
 }
+else
+{
 one = atoi(argv[1]);
 two = atoi(argv[2]);
 result = (one) * (two);
 printf("%d\n", result);
 return (0);
+}
 }
