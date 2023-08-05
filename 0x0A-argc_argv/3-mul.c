5794e0bf52aafca27eb648ee4a253807e2f53cd2
Darlington <obii6776@gmail.com>
2023-08-04 18:22:47 -0700

    gittt

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: param
+ * @argv: param
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+int one;
+int two;
+int result;
+
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+one = atoi(argv[1]);
+two = atoi(argv[2]);
+result = one * two;
+printf("%d\n", result);
+return (0);
+}
