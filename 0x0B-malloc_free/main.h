9d2a9fe85565f00ea689070aa827df52870353c8
Darlington <obii6776@gmail.com>
2023-08-08 15:55:18 -0700

    omoooooooooooooooo

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,4 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-void *loop_runner(char *x, char *y, char *memory_space);
+char *loop_runner(char *x, char *y, char *memory_space);
