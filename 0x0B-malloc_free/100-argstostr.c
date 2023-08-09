f0ea5e37211fbfa9c7452b498ad0fd3c44973cbc
Darlington <obii6776@gmail.com>
2023-08-08 18:24:40 -0700

    no betty

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *argstostr(int ac, char **av)
+{
+    int total_length, i, current_index;
+
+    if (ac == 0 || av == NULL)
+    {
+        return (NULL);
+    }
+
+    total_length = 0;
+    for (i = 0; i < ac; i++)
+    {
+        total_length += strlen(av[i]) + 1;
+    }
+
+    char *result = (char *)malloc(total_length * sizeof(char));
+    if (result == NULL)
+    {
+        return (NULL);
+    }
+
+    current_index = 0;
+    for (i = 0; i < ac; i++)
+    {
+        strcpy(result + current_index, av[i]);
+        current_index += strlen(av[i]);
+        result[current_index] = '\n';
+        current_index++;
+    }
+
+    return (result);
+}
