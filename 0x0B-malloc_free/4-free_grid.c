827f1f36757821e4473b011228c06067a29eaa87
Darlington <obii6776@gmail.com>
2023-08-08 18:05:32 -0700

    done

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - Entry point
+ * @grid: param
+ * @height: param
+ * Return: always 0
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[1]);
+}
+free(grid);
+}
