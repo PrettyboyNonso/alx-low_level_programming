571dc93ec769a380ee72cf6026bca7d0b83003a7
Darlington <obii6776@gmail.com>
2023-08-10 17:48:20 -0700

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+/**
+ * _calloc - Entry point
+ * @nmemb: param
+ * @size: param
+ * Return: pointer
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    if (size == 0 || nmemb == 0)
-    {
-        return NULL;
-    }
-    void *memory = malloc(size * nmemb);
-    if (memory == NULL)
-    {
-        return NULL;
-    }
-    for (int i = 0; i < nmemb; i++)
-    {
-        ((char *)memory)[i] = 0;
-    }
+	int i;
 
-    return memory;
+	if (size == 0 || nmemb == 0)
+	{
+	return (NULL);
+	}
+	void *memory = malloc(size * nmemb);
+
+	if (memory == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+	((char *)memory)[i] = 0;
+	}
+	return (memory);
 }
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,4 @@ void *malloc_checked(unsigned int b);
 char *check_else(char *ptr, int count_one, int n, char *s1, char *s2);
 char *check_if(char *ptr, int count_one, int count_two,  char *s1, char *s2);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
