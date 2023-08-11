0a48a41163bde7c441273f0ddf0a746ab77bfdf4
Darlington <obii6776@gmail.com>
2023-08-10 18:13:16 -0700

    one done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,46 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * string_nconcat - Entry point
- * @s1: param
- * @s2: param
- * @n:param
- * Return: pointer always
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_string;
-	unsigned int itteration_one, itteration_two;
-	int i;
-	int j;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	new_string = NULL;
-	itteration_one = 0;
-	itteration_two = 0;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (s1 == NULL)
-	{
-	s1 = "";
-	}
-	if (s2 == NULL)
-	{
-	s2 = "";
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	itteration_one += 1;
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	itteration_two += 1;
-	}
-	if (n >= itteration_two)
-	{
-	new_string = check_if(new_string, itteration_one, itteration_two,  s1, s2);
-	return (new_string);
-	}
-
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
 	{
-	new_string = check_else(new_string, itteration_one, n, s1, s2);
-	return (new_string);
+		s[i] = s1[i];
+		i++;
 	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
+
