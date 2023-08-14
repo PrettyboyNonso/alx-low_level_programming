29f43bc14f1ab6b61931f56a56bf0881db9034d3
Darlington <obii6776@gmail.com>
2023-08-14 14:54:39 -0700

    my bad

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - Entry point
  * @name: param
@@ -9,10 +10,12 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
+
 	if (name == NULL || owner == NULL)
 		return NULL;
 
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return NULL;
 
