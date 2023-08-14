052b086b9201dd731e501e3077a7f47e65bca5e9
Darlington <obii6776@gmail.com>
2023-08-14 15:08:31 -0700

    free

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,6 @@ char *owner;
 typedef struct dog dog_t;
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
