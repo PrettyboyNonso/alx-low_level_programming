8dec8621e1695bb30d1f6af014683fb043c37a99
Darlington <obii6776@gmail.com>
2023-08-14 13:46:55 -0700

    done now

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - Entry point
+ * @d: param
+ */
+void print_dog(struct dog *d)
+{
+       	printf("Name: %s\n", d->name);
+	printf("Age: %lf\n", d->age);
+	printf("Name: %s", d->owner);
+}
+
