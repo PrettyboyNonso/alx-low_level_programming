425d778fef91018eddb5f696c2bba2b7dc46ca20
Darlington <obii6776@gmail.com>
2023-08-16 11:22:12 -0700

    trying

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - Entry point
+ * @array: param
+ * @size: param
+ * @cmp: param
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+        return (-1);
+	}
+	if (cmp != NULL && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+		if ((cmp)(array[i]))
+		{
+			return (i);
+			break;
+		}
+		}
+	}
+	return (-1);
+}
