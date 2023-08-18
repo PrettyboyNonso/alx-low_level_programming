5b4d367c8fe0e6c7901387ed7be7282b990ed764
Darlington <obii6776@gmail.com>
2023-08-17 17:42:50 -0700

    done

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+/**
+ * print_strings - Entry point
+ * @separator: param
+ * @n: param
+ * Return: void
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	const char *str;
 	va_list strings;
+
 	va_start(strings, n);
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, const char *);
