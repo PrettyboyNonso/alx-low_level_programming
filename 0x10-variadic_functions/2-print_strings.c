ffe5c84d7ce7894a99a92f8775c7e11fb883c2f9
Darlington <obii6776@gmail.com>
2023-08-17 17:38:33 -0700

    failed betty

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	const char *str;
+	va_list strings;
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(strings, const char *);
+		if (str != NULL)
+		{
+			printf("%s", str);
+			if (separator != NULL && i != n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+		else if (str == NULL)
+		{
+			printf("(nil)");
+			if (separator != NULL && i != n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	printf("\n");
+}
