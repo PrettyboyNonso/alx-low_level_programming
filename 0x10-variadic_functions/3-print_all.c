cf42f66bc50f8f5dd9ba106a2137c10f1a6d0df7
Darlington <obii6776@gmail.com>
2023-08-17 18:06:52 -0700

    done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,55 @@
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+ * print_all - Entry point
+ * @format: param
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
 	int i;
-    char *s;
-    char current_format;
-    va_list args;
-    va_start(args, format);
+	char *s;
+	char current_format;
+	va_list args;
 
-i = 0;
-    while (format && format[i])
-    {
-        current_format = format[i];
-        switch (current_format)
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                s = va_arg(args, char *);
-                if (s == NULL)
-                {
-                    printf("(nil)");
-                }
-                else
-                {
-                    printf("%s", s);
-                }
-                break;
-            default:
-                break;
-        }
+	va_start(args, format);
 
-        if (format[i + 1] != '\0' && (current_format == 'c' || current_format == 'i' || current_format == 'f' || current_format == 's'))
-        {
-            printf(", ");
-        }
+	i = 0;
+	while (format && format[i])
+	{
+		current_format = format[i];
+		switch (current_format)
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s", s);
+				}
+				break;
+			default:
+				break;
+		}
+		if (format[i + 1] != '\0' && (current_format == 'c' || current_format == 'i' || current_format == 'f' || current_format == 's'))
+		{
+			printf(", ");}
+	
+		i++;
+	}
 
-        i++;
-    }
-
-    printf("\n");
-    va_end(args);
+	printf("\n");
+	va_end(args);
 }
-
