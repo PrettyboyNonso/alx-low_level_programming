e214ad0cb7ddaed581367c97079d6861d4c33e23
Darlington <obii6776@gmail.com>
2023-08-25 15:39:07 -0700

    almost done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.hi>
+/**
+ * print_list - Entry point
+ * @h: param
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *temp;
+	int count;
+
+	temp = h;
+	count = 0;
+
+	if (h != NULL)
+	{
+		while (temp->next != '\0')
+		{
+		if (temp->str == NULL)
+		{
+		printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] ", temp->len);
+			printf("%s\n", temp->str);
+		}
+
+		temp = temp->next;
+		count++;
+		}
+		if (temp->next == '\0')
+		{
+			if (temp->str == NULL)
+			{
+				printf("[0] (nil)\n");
+			}
+			else
+			{
+				printf("[%d] ", temp->len);
+				printf("%s\n", temp->str);
+			}
+			count++;
+		}
+	}
+	return (count);
+}
+
