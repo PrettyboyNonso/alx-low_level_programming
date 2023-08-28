85b480f5a0007ae9f454f3607e94bcde14c992a3
Darlington <obii6776@gmail.com>
2023-08-28 15:45:27 -0700

    I did it

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stddef.h>
+/**
+ * free_listint2 - Function entry
+ * @head: param
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *nextNodeAddress;
+
+	temp = *head;
+
+	if (temp != NULL)
+	{
+		while (temp != NULL)
+		{
+			nextNodeAddress = temp->next;
+			free(temp);
+			temp = nextNodeAddress;
+		}
+	}
+	*head = NULL;
+}
