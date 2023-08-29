fb438c8d8f32dcd669c34cb4a42d294401d95fbe
Darlington <obii6776@gmail.com>
2023-08-29 15:25:03 -0700

    number 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - Entry point
+ * @head: param
+ * @index: param
+ * Return: an int
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	int success;
+	unsigned int i;
+	listint_t *temp;
+	listint_t *beforeNode;
+
+	temp = *head;
+	beforeNode = *head;
+
+	if (temp != NULL)
+	{
+		if (index > 0)
+		{
+			for (i = 0; i < (index - 1); i++)
+			{
+				beforeNode = beforeNode->next;
+			}
+			for (i = 0; i < index; i++)
+			{
+				temp = temp->next;
+			}
+
+			beforeNode->next = temp->next;
+			free(temp);
+		}
+		else if (index == 0)
+		{
+			*head = temp->next;
+			free(temp);
+		}
+		success = 1;
+	}
+	else
+	{
+		success = -1;
+	}
+	return (success);
+}
