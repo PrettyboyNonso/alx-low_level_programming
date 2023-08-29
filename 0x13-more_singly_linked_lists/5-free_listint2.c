7ca1690963ec8299401e3d11b260b0cab02230e4
Darlington <obii6776@gmail.com>
2023-08-29 10:15:29 -0700

    tried it again

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,21 +9,17 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *nextNodeAddress;
 
-	temp = *head;
-
-	if (temp != NULL)
-	{
-		while (temp != NULL)
-		{
-			nextNodeAddress = temp->next;
-			free(temp);
-			temp = nextNodeAddress;
-		}
-	}else
-	{
+	if (head == NULL)
 		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	*head = temp;
+
+	*head = NULL;
+
 }
